f5e0912bfa46dcc93b807fb37e2930de16f7fb65
qzxin <qzxin.tmp@gmail.com>
2015-08-26 21:05:27 +0800

    changes log

--- quick-sort.cpp
+++ quick-sort.cpp
@@ -3,36 +3,61 @@ using namespace std;
 void swap(int &A, int &B) {
     int temp = A; A = B; B = temp;
 }
-int Partation(int nums[], int left, int right) {
-    int number = nums[left];
-    int i = left+1, j = right;
-    while(i < j) {
-        while (nums[i] < number)
-            i++;
-        while (nums[j] > number)
-            j--;
-        if (j <= i)
-            break;
-        swap(nums[i], nums[j]);
-        i++, j--;
-    }
-    swap(nums[left], nums[j]);
-    return j;
-}
-void QuickSort(int nums[], int left, int right) {
-    if (nums == NULL || left >= right)
+
+void QuickSort(int a[], int l, int r) {
+    // 直接交换法
+    if (a == NULL || l >= r)
         return;
-    int index = Partation(nums, left, right);
-    QuickSort(nums, left, index-1);
-    QuickSort(nums, index+1, right);
+    int pivot = a[l]; //选最左侧为基元
+    int i = l;
+    int j = r+1;
+    while (i < j) {
+        while (i < r && a[++i] < pivot);
+        while (a[--j] > pivot); // a[j]向左不会穿过a[l]
+        if (i >= j)
+            break;
+        swap(a[i], a[j]);
+    }
+    swap(a[j], a[l]); //右指针是基元交换的位置
+    QuickSort(a, l, j-1);
+    QuickSort(a, j+1, r);
+}
+
+void QuickSort2(int a[], int l, int r) {
+    // 挖坑填坑法
+    if(l >= r)
+        return;
+    int i = l;
+    int j = r;
+    int pivot = a[i]; // 左边先挖坑
+    while (i < j) {
+        while (i < j && a[j] > pivot)
+            j--;
+        if (i < j) {
+            a[i++] = a[j]; // 填左坑，挖右坑
+        }
+        while (i < j && a[i] < pivot)
+            i++;
+        if (i < j) {
+            a[j--] = a[i]; // 填右坑，挖左坑
+        }
+    }
+    a[j] = pivot; // 因为最先挖的左坑，最后填右坑
+    QuickSort2(a, l, j-1);
+    QuickSort2(a, j+1, r);
 }
 
 
 int main() {
-    int nums[] = {1,5,3,6,7,3,8,3,12,45,11};
-    int len = sizeof(nums)/sizeof(nums[0]);
-    QuickSort(nums, 0, len-1);
-    for (int i = 0; i < len; i++)
-        cout << nums[i] << " ";
+    int nums1[] = {1,5,3,6,7,3,3,3,3,8,5,5,5,3,12,45,11};
+    int nums2[] = {1,5,3,6,7,3,3,3,3,8,5,5,5,3,12,45,11};
+    int len = sizeof(nums1)/sizeof(nums1[0]);
+    QuickSort(nums1, 0, len-1);
+    QuickSort2(nums2, 0, len-1);
+    for (int i = 0; i < len; i++) 
+        cout << nums1[i] << " ";
+    cout << endl;
+    for (int i = 0; i < len; i++) 
+        cout << nums2[i] << " ";
     cout << endl;
 }
